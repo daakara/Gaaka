5d617e892063384ee457d438815b3b221364c647
David Akara <david@gaaka.com>
2025-12-30 03:32:09 +0100

    enhanced update v1.2

---
+++ wordpress.ts
@@ -0,0 +1,41 @@
+const API_URL = process.env.WORDPRESS_API_URL
+
+async function fetchAPI(query: string, { variables }: { variables?: any } = {}) {
+  const headers = { 'Content-Type': 'application/json' }
+
+  const res = await fetch(API_URL, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  })
+
+  const json = await res.json()
+  if (json.errors) {
+    console.error(json.errors)
+    throw new Error('Failed to fetch API')
+  }
+  return json.data
+}
+
+export async function getAllProducts() {
+  const data = await fetchAPI(`
+    query AllProducts {
+      products {
+        nodes {
+          id
+          title
+          productFields {
+            price
+            images {
+              mediaItemUrl
+            }
+          }
+        }
+      }
+    }
+  `)
+  return data?.products
+}
